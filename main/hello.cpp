5d8c2743e5effffa2789d1a67f5f5409d5e4fe08
AkshayC <akshayc@example.com>
2023-02-17 19:46:24 +0530

    Modified file

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
+  cout << "My name is Akshay\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
